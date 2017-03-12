43b8f86a38b9a8cb2c473d0038a6bd3e8acbe238
CRT13 <aeronfinium@gmail.com>
2017-03-12 16:56:20 +0530

    Create lab201a.asm
    Assembly Code: Evaluating aX^2 + bY^2 + cZ^2, where a,b,c are either multiples of 2, or differ from them by 1.

---
+++ ASM/Arithmetic/Lab2/lab201a.asm
@@ -0,0 +1,27 @@
+;Evaluate aX^2 + bY^2 + cZ^2 where a,b,c = 2n,2n-1, or 2n+1
+;           2X^2 + 5Y^2 + 7Z^2
+
+  AREA lab201a  CODE  READONLY
+  ENTRY
+ ;Load
+    MOV R0,#0
+    LDI R1,=X;
+    LDI R2,=Y;
+    LDI R3,=Z;
+ ;Logic
+    MUL R1,R1,R1        ;R1 <= X^2
+    MUL R2,R2,R2        ;R2 <= Y^2
+    MUL R3,R3,R3        ;R3 <= Z^2
+    ADD R1,R0,R1,LSL#1  ;R1 <= X^2
+    ADD R2,R2,R2,LSL#2  ;R2 <= 5Y^2
+    RSB R3,R3,R3,LSL#3  ;R3 <= 7X^2
+ ;Store
+    ADD R0,R1,R2
+    ADD R0,R0,R3
+    STR R0,Sum32        ;Stores result at address pointed by 'Sum32'
+ ;Variable Definitions
+ X DCB 1
+ Y DCB 2
+ Z DCB 3
+ Sum32 DCD 0
+  EXIT
